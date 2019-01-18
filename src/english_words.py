b90286d0f7cceed647fdd3e55d270c40015171c7
eda3 <gerorin1010@gmail.com>
2019-01-18 20:16:16 +0900

    add:Google image search

--- src/english_words.py
+++ src/english_words.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import sys
+import webbrowser
+from selenium import webdriver
 
 
 def main(args=sys.argv):
@@ -7,6 +9,15 @@ def main(args=sys.argv):
     if len(args) == 1:
         print("Add it to the first argument.")
         sys.exit()
+
+    # Google image search & save screenshot
+    url = 'https://www.google.com/search?q={}&tbm=isch'.format(args[1])
+    driver = webdriver.Chrome()
+    driver.get(url)
+    w = driver.execute_script('return document.body.scrollWidth')
+    h = driver.execute_script('return document.body.scrollHeight')
+    driver.set_window_size(w, h)
+    driver.save_screenshot('screenshot.png')
 
 
 if __name__ == "__main__":
