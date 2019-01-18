7efbe14ca2e1a431d916efc7a7d9065bbe020224
eda3 <gerorin1010@gmail.com>
2019-01-18 13:40:00 +0900

    add:Check arguments

--- english_words.py
+++ english_words.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+import sys
+
+
+def main(args=sys.argv):
+    # check arguments
+    if len(args) == 1:
+        print("Add it to the first argument.")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
