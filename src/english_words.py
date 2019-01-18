136f45b06819e36533aab50f4c51088642e2e48c
eda3 <gerorin1010@gmail.com>
2019-01-18 22:39:38 +0900

    add: upload image

--- src/english_words.py
+++ src/english_words.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+import os
 import sys
 from selenium import webdriver
+from gyazo import Api
+import json
 
 
 def main(args=sys.argv):
@@ -17,6 +20,26 @@ def main(args=sys.argv):
     h = driver.execute_script('return document.body.scrollHeight')
     driver.set_window_size(w, h)
     driver.save_screenshot('screenshot.png')
+
+    image_url = upload_image()
+    print(image_url)
+
+
+def upload_image():
+    if "API_TOKEN" not in os.environ:
+        print("command `export API_TOKEN=hoge`")
+
+    API_TOKEN = os.environ.get("API_TOKEN")
+    client = Api(access_token=API_TOKEN)
+
+    # Upload an image
+    with open('screenshot.png', 'rb') as f:
+        image = client.upload_image(f)
+        image_json = image.to_json()
+        image_dict = json.loads(image_json)
+        image_url = image_dict['url']
+
+        return image_url
 
 
 if __name__ == "__main__":
