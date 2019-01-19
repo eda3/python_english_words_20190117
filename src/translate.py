6879d3f253bfc3d483aa543d512a1cfba43bc4e0
eda3 <gerorin1010@gmail.com>
2019-01-19 19:21:32 +0900

    fix: Change from argument to execution

--- src/translate.py
+++ src/translate.py
@@ -1,37 +1,11 @@
+#!/usr/local/bin/python3
+import sys
 from urllib.request import urlopen
 import json
 import click
-import re
+from meaning import Meaning
 
 BASE_URL = "https://glosbe.com/gapi/translate"
-
-
-class Meaning:
-    def __init__(self, phrase, japanese, english=None):
-        self.phrase = phrase
-        self.japanese = japanese
-        self.english = english
-
-    def look_up(self):
-        print("* " + self.japanese + ' *')
-        if self.english:
-            self._extract_mean()
-        else:
-            print("適切な英語の説明がありませんでした。")
-
-    def _extract_mean(self):
-        for i, mean in enumerate(self.english, 1):
-            text = mean['text']
-            eg_regex = re.search('e.g.', text)
-            esp_regex = re.search('esp.', text)
-            if not any([eg_regex, esp_regex]):
-                text = text.replace('. ', '.\n\t')
-            else:
-                text = mean['text']
-
-            if mean['language'] == 'ja':
-                continue
-            print(f"意味{i} : {text}")
 
 
 def get_url(phrase):
@@ -54,15 +28,9 @@ def load_json(json_data):
         yield json_dict
 
 
-@click.command()
-@click.option('--phrase', '-p', default='python', help='検索したい文字列が必要です')
-def cmd(phrase):
-    num = click.prompt('検索結果の出力数(1〜7)を入力して下さい', type=int)
-    if num == 0:
-        raise click.BadParameter("出力数は1以上にしてください")
-    if num > 7:
-        raise click.BadParameter("そんなに出力できません。")
+def main(phrase=sys.argv[1]):
 
+    num = 3
     url = get_url(phrase)
     response = urlopen(url)
     json_data = response.read().decode("utf-8")
@@ -83,10 +51,6 @@ def cmd(phrase):
         mean = Meaning(phrase, japanese, english)
         mean.look_up()
         print("=================================")
-
-
-def main():
-    cmd()
 
 
 if __name__ == '__main__':
